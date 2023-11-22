56059ccf824d47f90b442586fe0323febfe41f12
Juls Lin <julianna.lin4@gmail.com>
2023-11-22 15:28:29 -0800

    can open reminder page on web

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -1,19 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
-  ensureAuthenticated: function (req, res, next) {
-    if (req.isAuthenticated()) {
-      return next();
-    }
-    res.redirect("/auth/login");
-  },
-  forwardAuthenticated: function (req, res, next) {
-    if (!req.isAuthenticated()) {
-      return next();
-    }
-    res.redirect("/dashboard");
-  },
-};
-=======
     ensureAuthenticated: function (req, res, next) {
       if (req.isAuthenticated()) {
         return next();
@@ -28,4 +13,3 @@ module.exports = {
     },
   };
   
->>>>>>> julianna
